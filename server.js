3124a6f7bf452a266a549897145273e5cf6400ea
Abhishek4298 <abhishek.bhavsar@bacancytechnology.com>
2020-12-18 14:15:18 +0530

    getting single book and author and also list of all books and authors

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const {
 } = require('graphql')
 const app = express();
 
-
 const authors = [
 	{ id: 1, name: 'J. K. Rowling' },
 	{ id: 2, name: 'J. R. R. Tolkien' },
@@ -30,7 +29,27 @@ const BookType = new GraphQLObjectType({
 		id: { type: GraphQLNonNull(GraphQLInt) },
 		name: { type: GraphQLNonNull(GraphQLString) },
 		authorId: { type: GraphQLNonNull(GraphQLInt) },
-	
+		author: {
+			type: AuthorType,
+			resolve: (book) => {
+				return authors.find(author => author.id === book.authorId)
+			}
+		}
+	})
+})
+
+const AuthorType = new GraphQLObjectType({
+	name: 'Author',
+	description: 'This represents a author of a book',
+	fields: () => ({
+		id: { type: GraphQLNonNull(GraphQLInt) },
+		name: { type: GraphQLNonNull(GraphQLString) },
+		books: {
+			type: new GraphQLList(BookType),
+			resolve: (author) => {
+				return books.filter(book => book.authorId === author.id)
+			}
+		}
 	})
 })
 
@@ -39,17 +58,39 @@ const RootQueryType = new GraphQLObjectType({
 	name: 'Query',
 	description: 'Root Query',
 	fields: () => ({
+		book: {
+			type: BookType,
+			description: 'A Single Book',
+			args: {
+				id: { type: GraphQLInt }
+			},
+			resolve: (parent, args) => books.find(book => book.id === args.id)
+		},
 		books: {
 			type: new GraphQLList(BookType),
 			description: 'List of All Books',
 			resolve: () => books
 		},
+		authors: {
+			type: new GraphQLList(AuthorType),
+			description: 'List of All Authors',
+			resolve: () => authors
+		},
+		author: {
+			type: AuthorType,
+			description: 'A Single Author',
+			args: {
+				id: { type: GraphQLInt }
+			},
+			resolve: (parent, args) => authors.find(author => author.id === args.id)
+		}
 	})
 })
 
 const schema = new GraphQLSchema({
 	query: RootQueryType,
 })
+
 app.use('/graphql', graphqlHTTP({
 	schema: schema,
 	graphiql: true
